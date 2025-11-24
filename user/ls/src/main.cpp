1e96ec40c6feb3c70d1342e88ba1762c390524dd
Polina Redina <prc6@st-andrews.ac.uk>
2025-11-24 17:53:52 +0000

    P3 added sorting by name or size

--- user/ls/src/main.cpp
+++ user/ls/src/main.cpp
@@ -4,9 +4,16 @@
 
 using namespace stacsos;
 
+// types of sorting of directory entries
+enum class sort_mode {
+	none, // no sorting (random order)
+	name, // sort by name (alphabetically)
+	size // sort by file size (smallest to largest)
+};
+
 // if error occurs, print accepted usage
 static void print_usage() {
-	console::get().write("error: usage: ls [-l] <path>\n");
+	console::get().write("error: usage: ls [-l or -n or -s or -ln or -ls] <path>\n");
 }
 
 // main method to return the list of directories
@@ -20,18 +27,58 @@ int main(const char *cmdline) {
     // tracker for if its necessary to print the extra file info (size and type)
 	bool long_mode = false;
 
+	// tracker for sorting mode (default none)
+	sort_mode mode = sort_mode::none;
+
 	// skip any leading spaces
 	while (*cmdline == ' ') {
 		++cmdline;
 	}
 
-	// check for optional "-l" flag
+	// parse flags, allowed:
+	// "-l" long mode
+	// "-n" sort by name
+	// "-s" sort by size
+	// "-ln" and "-ls" combinations accepted 
 	if (*cmdline == '-') {
 		++cmdline;
-		if (*cmdline == 'l') {
-			long_mode = true; // switch tracker to true
-			++cmdline;
-		} else { // if invalid flag, print accepted usage
+		
+		char flag1 = 0;
+		char flag2 = 0;
+
+		// read only max 2 flags
+		while(*cmdline && *cmdline != ' ') {
+			char flag = *cmdline++;
+
+			if (flag1 == 0) {
+				flag1 = flag;
+			} else if (flag2 == 0) {
+				flag2 = flag;
+			} else { // if more than 2 flag chars given, print accepted usage
+				print_usage();
+				return 1;
+			}
+		}
+
+		// interpret flags to check if combo is valid
+		if (flag1 == 'l') {
+			long_mode = true;
+			if (flag2 == 0) {
+				// just -l
+			} else if (flag2 == 'n') { // -ln
+				mode = sort_mode::name;
+			} else if (flag2 == 's') { // -ls
+				mode = sort_mode::size;
+			} else { // invalid combo after -l
+				print_usage();
+				return 1;
+			}
+		} else if (flag1 == 'n' && flag2 == 0) { // -n
+			mode = sort_mode::name;
+		} else if (flag1 == 's' && flag2 == 0) { // -s
+			mode = sort_mode::size;
+		} else {
+			// invalid combo
 			print_usage();
 			return 1;
 		}
@@ -74,6 +121,30 @@ int main(const char *cmdline) {
 
 	// get number of entries
 	u64 count = res.length;
+
+	// sort by name or size if flags given
+	// using selection sort O(n^2)
+	if (mode == sort_mode::name) {
+		for (u64 i = 0; i + 1 < count; ++i) {
+			for (u64 j = i + 1; j < count; ++j) {
+				if (memops::strcmp(entries[i].name, entries[j].name) > 0) {
+					directory_entry tmp = entries[i];
+					entries[i] = entries[j];
+					entries[j] = tmp;
+				}
+			}
+		}
+	} else if (mode == sort_mode::size) {
+		for (u64 i = 0; i + 1 < count; ++i) {
+			for (u64 j = i + 1; j < count; ++j) {
+				if (entries[i].size > entries[j].size) { // smallest to biggest
+					directory_entry tmp = entries[i];
+					entries[i] = entries[j];
+					entries[j] = tmp;
+				}
+			}
+		}
+	}
 
     // iterate over list of entries, printing each with appropriate info (depending on flag)
 	for (u64 i = 0; i < count; ++i) {
